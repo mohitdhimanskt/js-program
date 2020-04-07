1ae489a470e33568fb62952039e27e6be48634a3
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-04-07 21:58:48 +0530

    created two function

---
+++ Therecipecard/index.js
@@ -0,0 +1,8 @@
+<script>
+var  recipe ={
+    'title': 'Meal name',
+    'servings': 2,
+    'ingredients':['4 eggs','2 strips of bacon','1 tsp salt/pepper']
+};
+    
+</script>
---
+++ removeTheComma/index.js
@@ -0,0 +1,3 @@
+
+let myString ="hello,this,is,a,difficult,to,read,sentence";
+let  result = myString.replace(/,/g,"");
